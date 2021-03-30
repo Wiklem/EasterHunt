4153e81638a0ff4a155dc09727dfc7e7fe597061
Wiklem <admin@wiklem.no>
2021-03-30 15:36:46 +0200

    Vise melding om å godta lokasjon

--- src/context/EasterContext.tsx
+++ src/context/EasterContext.tsx
@@ -3,6 +3,7 @@ import Loading from "../components/Loading";
 import { ILocation } from "../utils/types";
 import { LoadScript } from "@react-google-maps/api";
 import API from "../utils/keys";
+import { Alert } from "antd";
 
 interface IEasterContext {
   currentPosition: ILocation | null;
@@ -13,6 +14,7 @@ export const EasterContext = React.createContext<IEasterContext>({
 
 const EasterContextProvider: React.FC = ({ children }) => {
   const [currentPosition, setCurrentPosition] = React.useState<ILocation>();
+  const [error, setError] = React.useState<string>();
 
   const success = (position: any) => {
     const currentPosition = {
@@ -22,14 +24,56 @@ const EasterContextProvider: React.FC = ({ children }) => {
     setCurrentPosition(currentPosition);
   };
 
+  const gotError = (e: any) => {
+    if (e.code === 1) {
+      setError(
+        "Du må tilate nettsiden å bruke din posisjon for å bruke påskejakten. "
+      );
+    } else {
+      setError("Klarte ikke å hente enhetens posisjon.");
+    }
+  };
+
   React.useEffect(() => {
     const interval = setInterval(() => {
-      navigator.geolocation.getCurrentPosition(success);
+      navigator.geolocation.watchPosition(success, gotError, {
+        enableHighAccuracy: true,
+      });
     }, 1000);
     return () => clearInterval(interval);
   });
 
-  if (!currentPosition) return <Loading />;
+  if (error)
+    return (
+      <Alert
+        message={error}
+        type={"error"}
+        showIcon
+        description={
+          "Siden trenger posisjon for å navigere deg fram til påskeeggene."
+        }
+      />
+    );
+  if (!currentPosition)
+    return (
+      <div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Alert
+            style={{ width: "500px" }}
+            message={
+              "Du må tillate siden å bruke din posisjon for å bruke Påskejakten.no"
+            }
+            showIcon
+          />
+        </div>
+        <Loading />
+      </div>
+    );
   return (
     <EasterContext.Provider
       value={{
